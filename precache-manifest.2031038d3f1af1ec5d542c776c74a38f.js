01d3538f093dfce04965b7f67fd387953bb0447d
ladleo <rwelamila.jr@gamil.com>
2019-10-18 17:07:06 +0300

    Updates

---
+++ precache-manifest.2031038d3f1af1ec5d542c776c74a38f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d76490672e53547323f7cfb22dd3cc9",
+    "url": "/ladleo/ladleo.github.io/index.html"
+  },
+  {
+    "revision": "cf2cc4ed9afc624eded4",
+    "url": "/ladleo/ladleo.github.io/static/css/main.bf9fe460.chunk.css"
+  },
+  {
+    "revision": "b2f6cf1078f8a68341e5",
+    "url": "/ladleo/ladleo.github.io/static/js/2.cf6187b2.chunk.js"
+  },
+  {
+    "revision": "cf2cc4ed9afc624eded4",
+    "url": "/ladleo/ladleo.github.io/static/js/main.02d35c06.chunk.js"
+  },
+  {
+    "revision": "31c11a933ca40909e17f",
+    "url": "/ladleo/ladleo.github.io/static/js/runtime-main.33873796.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ladleo/ladleo.github.io/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "d0f9d65e02b4e7850bfb58b9af9403d3",
+    "url": "/ladleo/ladleo.github.io/static/media/me.d0f9d65e.png"
+  }
+]);
