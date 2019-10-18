782b6cb5c48b454db89a98349dc6d76705f9f9a5
ladleo <rwelamila.jr@gamil.com>
2019-10-18 17:13:39 +0300

    Updates

---
+++ precache-manifest.4148a57a3ac6e558940d53f370d9ec7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ef0fa5ed5d1b25accb29bad731cf25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d03734b8e8fed38073e6",
+    "url": "/static/css/main.bf9fe460.chunk.css"
+  },
+  {
+    "revision": "b2f6cf1078f8a68341e5",
+    "url": "/static/js/2.cf6187b2.chunk.js"
+  },
+  {
+    "revision": "d03734b8e8fed38073e6",
+    "url": "/static/js/main.7e74d29b.chunk.js"
+  },
+  {
+    "revision": "3732e1b27cff612958d2",
+    "url": "/static/js/runtime-main.a806e91b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "d0f9d65e02b4e7850bfb58b9af9403d3",
+    "url": "/static/media/me.d0f9d65e.png"
+  }
+]);
