2d1f7a6ffd96c550c0a9c4922ff868b3cb77fb54
ladleo <rwelamila.jr@gmail.com>
2020-01-17 10:46:53 +0300

    Updates

---
+++ precache-manifest.7ec45f2448e11f6273d538a8fffdfdcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3824583ddd3c3399917bd0ce1f117854",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ef78a9fd4d0a946049e",
+    "url": "/static/css/main.b321ea83.chunk.css"
+  },
+  {
+    "revision": "b193b6fbed818712f0a1",
+    "url": "/static/js/2.a0a322b8.chunk.js"
+  },
+  {
+    "revision": "9ef78a9fd4d0a946049e",
+    "url": "/static/js/main.57dbeb7a.chunk.js"
+  },
+  {
+    "revision": "486179fd78d367843b28",
+    "url": "/static/js/runtime-main.1b109837.js"
+  },
+  {
+    "revision": "d0f9d65e02b4e7850bfb58b9af9403d3",
+    "url": "/static/media/me.d0f9d65e.png"
+  }
+]);
