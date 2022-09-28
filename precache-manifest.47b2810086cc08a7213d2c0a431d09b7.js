a1257e3007b0e27b92cf6a9481e82595accead14
Denis Rwelamila <denis@ramani.io>
2022-09-28 12:51:59 +0300

    Updates

---
+++ precache-manifest.47b2810086cc08a7213d2c0a431d09b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e91d3cf734961f629a7e6add5ded02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ab06f5f64a275deadf",
+    "url": "/static/css/main.394e9547.chunk.css"
+  },
+  {
+    "revision": "660554e885589c5b71e3",
+    "url": "/static/js/2.385bb059.chunk.js"
+  },
+  {
+    "revision": "c5ab06f5f64a275deadf",
+    "url": "/static/js/main.c7d27fcd.chunk.js"
+  },
+  {
+    "revision": "3732e1b27cff612958d2",
+    "url": "/static/js/runtime-main.a806e91b.js"
+  },
+  {
+    "revision": "b20496d849a95bec6be96cd53e2e7bd6",
+    "url": "/static/media/me.b20496d8.png"
+  }
+]);
